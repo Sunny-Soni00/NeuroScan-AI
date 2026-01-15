23377d28afbcf67c368cc2a5273d304cdcec57bc
NeuroScan Developer <dev@neuroscan.ai>
2026-01-15 18:49:52 +0000

    init

--- download_sample.py
+++
@@ -1,6 +0,0 @@
-import kagglehub, os
-
-base = kagglehub.dataset_download('aryashah2k/brain-tumor-segmentation-brats-2019')
-for root, _, files in os.walk(base):
-    for f in files:
-        print(os.path.join(root, f))
